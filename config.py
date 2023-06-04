2aac6d9fa84d70379ab6b666e38c64fa3a7f6d60
SATYAM <126665079+Raichuop07@users.noreply.github.com>
2023-06-04 23:02:31 +0530

    Update config.py

--- config.py
+++ config.py
@@ -21,12 +21,12 @@ OWNER_ID = list(map(int, getenv("OWNER_ID", "1356469075").split()))
 HEROKU_API_KEY = getenv("HEROKU_API_KEY", None)
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", None)
 
-UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/Raichuop07/MENTALIC")
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/Raichuop07/Hero")
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/FallenAssociation")
-SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/DevilsHeavenMF")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/Thealtron")
+SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/altronchats")
 
 SUPPORT_HEHE = SUPPORT_GROUP.split("me/")[1]
 
@@ -76,11 +76,11 @@ clean = {}
 autoclean = []
 
 
-START_IMG_URL = getenv("START_IMG_URL", "https://te.legra.ph/file/05d92b7f116124cc2e6b2.jpg")
+START_IMG_URL = getenv("START_IMG_URL", "https://telegra.ph/file/93ac380bc3669ecc32750.jpg")
 
 PING_IMG_URL = getenv(
     "PING_IMG_URL",
-    "https://te.legra.ph/file/05d92b7f116124cc2e6b2.jpg",
+    "https://telegra.ph/file/0e5c3cc77e86b36a14151.jpg",
 )
 
 PLAYLIST_IMG_URL = "https://te.legra.ph/file/4ec5ae4381dffb039b4ef.jpg"
@@ -130,9 +130,9 @@ if UPSTREAM_REPO:
 if PING_IMG_URL:
     if PING_IMG_URL != "assets/Ping.jpeg":
         if not re.match("(?:http|https)://", PING_IMG_URL):
-            PING_IMG_URL = "https://te.legra.ph/file/05d92b7f116124cc2e6b2.jpg"
+            PING_IMG_URL = "https://telegra.ph/file/0e5c3cc77e86b36a14151.jpg"
 
 if START_IMG_URL:
     if START_IMG_URL != "assets/Ping.jpeg":
         if not re.match("(?:http|https)://", START_IMG_URL):
-            START_IMG_URL = "https://te.legra.ph/file/05d92b7f116124cc2e6b2.jpg"
+            START_IMG_URL = "https://telegra.ph/file/93ac380bc3669ecc32750.jpg"
